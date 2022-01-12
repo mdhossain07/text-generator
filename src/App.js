39a138807187eee3fd8b5dc1d45a3d91e93fa6cd
mdhossain07 <mrabir8097@gmail.com>
2022-01-12 20:47:52 +0600

    text-generator-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './data';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+  
+    if (count <= 0){
+      amount = 1;
+    }
+
+    if (count > 8){
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h2 className='title'> Tired Of Boring Lorem Ipsum? </h2>
+
+      <form onSubmit={handleSubmit}>
+
+      <label htmlFor="amount">
+        Paragraphs: 
+      </label>
+
+      <input type="number" name="amount" id="amount" 
+      value = {count} 
+      onChange={(e) => setCount(e.target.value)}/>
+
+      <button type='submit'> Generate </button>
+      </form>
+
+      <article className='content'>
+        {
+          text.map((item,index) => 
+          <p style={{marginBottom : '25px', color: '#667D98', fontSize: '14px', letterSpacing : '1px', lineHeight: '22px'}}
+          key = {index}> 
+          {item} </p>)
+        }
+      </article>
+    </main>
   );
 }
 
